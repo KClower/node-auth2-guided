65c260685168954b6df662a332e43a923c5ecdd0
KClower <kclower3@gmail.com>
2024-08-08 13:19:18 -0500

    finished quided project

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,14 +1,35 @@
 // AUTHENTICATION
+
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // something wrong with token
+        res.status(401).json({ you: "can't touch this" })
+      } else {
+        // token is good, we can see the data inside the decodedToken
+        req.jwt = decodedToken;
+
+        next()
+      }
+    })
+
+  } else {
+    res.status(401).json({ you: "shall not pass!" })
+  }
+
 }
 
-// AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
-}
+
+
+
+
 
 module.exports = {
   restricted,
-  checkRole,
+
 }
--- api/server.js
+++ api/server.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 
 const authRouter = require('./auth/auth-router.js')
 const usersRouter = require('./users/users-router.js')
+const { restricted } = require('./auth/auth-middleware.js');
+const checkRole = require('./auth/check-role-middleware.js');
+
 
 const server = express()
 
@@ -12,7 +15,7 @@ server.use(express.json())
 server.use(cors())
 
 server.use('/api/auth', authRouter)
-server.use('/api/users', usersRouter)
+server.use('/api/users', restricted, checkRole("admin"), usersRouter)
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'))
